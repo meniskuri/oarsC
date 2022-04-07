5841df6213d923cfd39407a8c18616424b554e47
Meniskuri <giorgikapanadze9169@gmail.com>
2022-04-07 17:34:31 +0400

    calculator chemit - dawyeba

---
+++ me_first_/first_C_tutorial/Cstart/CsamiSaati_amocanebi/gagrdzeleba2.c
@@ -0,0 +1,15 @@
+// Building a better calculator and other staff
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <math.h>
+#include <conio.h>
+
+int main()
+{
+  // ჩემით უნდა გავაკეთო ეს კალკულატორი. შვადარო ვიდეოს კალკულატორს. ვნახოთ შეიძლება საღამოს ჩავუჯდე. 
+
+  return 0;
+}
