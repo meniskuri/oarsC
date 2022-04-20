941f01d08b38504af63b24a5d7eee54505c8834f
Meniskuri <giorgikapanadze9169@gmail.com>
2022-04-20 19:49:56 +0400

    გამოცნობანა - წინასწარმეტყველების თამაში - შუაგზაშივარ

--- me_first_/first_C_tutorial/Cstart/CsamiSaati_amocanebi/gagrdzeleba3.c
+++ me_first_/first_C_tutorial/Cstart/CsamiSaati_amocanebi/gagrdzeleba3.c
@@ -1,62 +1,106 @@
-
-//gagrdzeleba 3 - struct - ebidan - video განვაგრძო
+// gagrdzeleba 3 - struct - ebidan - video განვაგრძო - Struct_totirial შია struct ების მაგალითები
+// Buildgin a Guessing Game (loop ებით) ვნახოთ რას აკეთებს და მეთვითნ გავაკეთო
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <math.h>
+#include <conio.h>
+#include <stdbool.h>
 #include <string.h>
+#include <errno.h>
 
-struct Jadoqrebi // დიდი ასაოთი ვიწყებთ რატომღაც. ალბათ რომ ვიცნოტ მერე სტრუქტურაა
+int parametrebi_Brzanebis(char* brzaneba)
 {
-  // ჩემი ჯადოქრები
-  char saxeli[100];
-  char jadoqruli_saxeli[100];
-  char dzalebi[100];
-  char joxis_saxeli[100];
-  char totemi[100];
-  int gafidvis_lvl;
-}; // წერტილ მძიმეი მთავრდება
+  /* ფუნქციის აღწერა:
+  აბრუნებს ლოგიკურ ერთიანს თუ ჩარების მასივი შედგება მხოლოდ ციფრებისგან
+  თუ არა აბრუნებს ლოგიკურ ნულიანს. ამოწმებს 2.3 ში წერტილ სიმბოლოს.
+  არ დაუშვებს ორ წერტილს. წერტილით დაწყებას.
+  */
 
+  bool cifria = false; // თუ მასივში ციფრები წერია გათრუვდება
+  int len, counter_par = 0, counter_mdzime = 0;
 
-void jadoqrebi_info_prnt (struct Jadoqrebi *jadoqrebi) // მისამართს მივცემ
-{
-  printf("================================ \n");
-  printf("pntr print \n");
-  printf("ჯადოქრის სახელი:           %s \n",jadoqrebi->saxeli);
-  printf("ჯადოქრული სახელი:          %s \n",jadoqrebi->jadoqruli_saxeli);
-  printf("ჯადოქრის ძალები:           %s \n",jadoqrebi->dzalebi);
-  printf("ჯადოქრის ტოტემი:           %s \n",jadoqrebi->totemi);
-  printf("ჯადოქრის გაფიდვის ლეველი: %d \n",jadoqrebi->gafidvis_lvl);
+  len = strlen(brzaneba);
+  printf("len = %d \n",len);
+
+  for (int i = 0; i <= len; i++)
+  {
+    // printf("brzaneba(d)[%d] %d %c \n",i,brzaneba[i],brzaneba[i]);
+    // printf("i = %d \n", i);
+    if (brzaneba[0] >= 48 && brzaneba[0] <= 57)
+    {
+      if ((brzaneba[i] >= 48 && brzaneba[i] <= 57 || brzaneba[i] == 46) || brzaneba[i] == 0)
+      {
+        cifria = true;
+      } else
+      {
+        cifria = false;
+        break;
+      }
+      if (brzaneba[i] == 46)
+      {
+        // ითვლის წერტილების რაოდენობას 3.3 ერთი წერტილია, 3.3.3 - ორი წერტილია. დაბლ რიცხვების შეყვანისთვის
+        counter_mdzime++;
+      }
+
+      if (counter_mdzime > 1)
+      {
+        // printf("counter_mdzime %d \n", counter_mdzime);
+        cifria = false;
+      }
+    }
+    counter_par++;
+  }
+  // printf("counter_par = %d \n",counter_par);
+  // printf("cifria = %d \n", cifria);
+  return cifria;
 }
 
-void jadoqrebi_info (struct Jadoqrebi jadoqrebi) // სახელს მივცემ
-{
-  printf("ჯადოქრის სახელი:           %s \n",jadoqrebi.saxeli);
-  printf("ჯადოქრული სახელი:          %s \n",jadoqrebi.jadoqruli_saxeli);
-  printf("ჯადოქრის ძალები:           %s \n",jadoqrebi.dzalebi);
-  printf("ჯადოქრის ტოტემი:           %s \n",jadoqrebi.totemi);
-  printf("ჯადოქრის გაფიდვის ლეველი: %d \n",jadoqrebi.gafidvis_lvl);
-}
 
 int main()
 {
-  struct Jadoqrebi ojaxi_da;
-  struct Jadoqrebi *ojaxi_da_pntr;
-  ojaxi_da_pntr = &ojaxi_da;
+  double secret = 1783; // გამოსაცნობი რიცხვი
+  double mixvedra;   // კლავიატურიდან შეყვანილი რიცხვს სადაც ჩავწე და შევადარებ secret-ს
 
-  strcpy(ojaxi_da.saxeli,"Shoshi");
-  strcpy(ojaxi_da.jadoqruli_saxeli,"SunGolden Dragon");
-  strcpy(ojaxi_da.dzalebi,"Sun Light and Green Trees Power Earth Power");
-  strcpy(ojaxi_da.totemi,"Leopard");
-  ojaxi_da.gafidvis_lvl = 28;
+  char brzaneba[51]; // სტრინგი რომელიც შემყავს კლავიატურიდან ვამოწმებ რიცხვია თუა არა და ვანიჭებ mixvedra-ს
+  bool brzanebaChek; //  თუ ფუნქციამ დააბრუნა 1 ანუ რიცხვი შევიყვანეთ თუარადა დააბრუნებს 0 ს
+  char *eptr;        // ??? string pointer რაც გადაყავს დაბლში (მგონი)
 
-  jadoqrebi_info(ojaxi_da);
-  jadoqrebi_info_prnt(ojaxi_da_pntr);
+  printf("================================== \n");
+  printf("ვთამაშობთ გამოცნობანას - წინასწარმეტყველების თამაში \n");
+  printf("შეყვანილია საიდუმლო secret ცვლადი, რომელიც უნდა გამოვიცნოთ \n");
 
-  int index = 6;
-  do {
-    printf("index = %d \n", index);
-    index++;
-  } while (index <= 5);
-  printf("index = %d \n", index);
+  while (true)
+  {
+    printf("================================== \n");
+    printf("შეიყვანეთ ნებისმიერი რიცხვი \n");
+    gets(brzaneba);
+    brzanebaChek = parametrebi_Brzanebis(brzaneba);
+
+    if (brzanebaChek == 1)
+    {
+      mixvedra = strtod(brzaneba, &eptr);
+      printf("mixvedra = %f \n",mixvedra);
+
+      if (mixvedra == secret)
+      {
+        printf("თამაში დამთვრებულია \n");
+        break;
+      } else if (mixvedra < secret) {
+        printf("========== \n");
+        printf("ცივა \n");
+      } else {
+        printf("========== \n");
+        printf("ცხელა \n");
+      }
+    } else
+    {
+      printf("================================== \n");
+      printf("ERROR - შეიყვანეთ რიცხვი \n");
+    }
+  }
 
   return 0;
 }
@@ -66,4 +110,10 @@ int main()
 არჩილმა დამირეკა და მითხრა რომ ნუ აძინებ ტვინსო
 როცა ტვინი არ გყავს დაძინებული მაშინ გამოგივა ყველაფერიო
 ნუ გუდავ ტვინსო. ისე როცა არ გყავს გაგაუდული ტვინი.. ტვინი თვითონ ვერ გაჩერდებაო
+
+ვბრაზობ რომ ნელა მივდივარ წინ.
+გაიხსენე ლაი. ნელა სიარულით შორს წავა.
+ასე რომ იარე თუნდაც ნელა. მაგრამ იარე.
+ბოლოს ფოფხვაც რომ მოგწიოს გაიხსენე სიზმარი..
+რომ უნდა იარო..
 */
