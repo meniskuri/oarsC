b59b6a5a4522971ac57ba2c1616b4f90ba05ec71
Meniskuri <giorgikapanadze9169@gmail.com>
2022-04-20 14:04:07 +0400

    Anglaxel and Anguriel Black and Golden Dragons - და ძმა

--- me_first_/first_C_tutorial/Cstart/CsamiSaati_amocanebi/testingChemi.c
+++ me_first_/first_C_tutorial/Cstart/CsamiSaati_amocanebi/testingChemi.c
@@ -29,48 +29,62 @@ void printMagicWandsInfo( struct MagicWands wand )
 
 int main( ) {
    // Struct version 1 - Defining a Structure
+   printf("====================================================== \n");
+   printf("Struct version 1 - Defining a Structure \n");
+
    struct Books Book1;        /* Declare Book1 of type Book */
    struct Books Book2;        /* Declare Book2 of type Book */
 
    /* book 1 specification */
-   strcpy( Book1.title, "Sons of Hurin"); // ჯოხების საცავისთვის იდეალურია
-   strcpy( Book1.author, "Tolkien");
-   strcpy( Book1.subject, "Dragons");
+   strcpy(Book1.title, "Sons of Hurin"); // ჯოხების საცავისთვის იდეალურია
+   strcpy(Book1.author, "Tolkien");
+   strcpy(Book1.subject, "Dragons");
    Book1.book_id = 1;
 
    /* book 2 specification */
-   strcpy( Book2.title, "Lord of the Rings - Return of the King");
-   strcpy( Book2.author, "Tolkien");
-   strcpy( Book2.subject, "New Reality");
+   strcpy(Book2.title, "Lord of the Rings - Return of the King");
+   strcpy(Book2.author, "Tolkien");
+   strcpy(Book2.subject, "New Reality");
    Book2.book_id = 2;
 
    /* print Book1 info */
-   printf( "Book 1 title   : %s\n", Book1.title);
-   printf( "Book 1 author  : %s\n", Book1.author);
-   printf( "Book 1 subject : %s\n", Book1.subject);
-   printf( "Book 1 book_id : %d\n", Book1.book_id);
+   printf("Book 1 title   : %s\n", Book1.title);
+   printf("Book 1 author  : %s\n", Book1.author);
+   printf("Book 1 subject : %s\n", Book1.subject);
+   printf("Book 1 book_id : %d\n", Book1.book_id);
 
    /* print Book2 info */
-   printf( "Book 2 title   : %s\n", Book2.title);
-   printf( "Book 2 author  : %s\n", Book2.author);
-   printf( "Book 2 subject : %s\n", Book2.subject);
-   printf( "Book 2 book_id : %d\n", Book2.book_id);
+   printf("Book 2 title   : %s\n", Book2.title);
+   printf("Book 2 author  : %s\n", Book2.author);
+   printf("Book 2 subject : %s\n", Book2.subject);
+   printf("Book 2 book_id : %d\n", Book2.book_id);
 
 
 
    // Struct version 2 - Structures as Function Arguments
-   /* book 1 specification */
-   struct MagicWands wand1;        /* Declare Book1 of type Book */
+   printf("====================================================== \n");
+   printf("Struct version 2 - Structures as Function Arguments \n");
+   printf("Magic Wands info - kapANA \n");
 
-   strcpy( wand1.title, "Anglaxel");
-   strcpy( wand1.author, "KapANA");
-   strcpy( wand1.owner, "KapANA");
-   strcpy( wand1.soul, "Black Dragon");
+   struct MagicWands wand1;        /* Declare Book1 of type Book */
+   struct MagicWands wand2;
+
+   strcpy(wand1.title, "Anglaxel");
+   strcpy(wand1.author, "KapANA");
+   strcpy(wand1.owner, "KapANA");
+   strcpy(wand1.soul, "Black Dragon");
    wand1.wand_id = 1;
 
-   /* print Book1 info */
-  printMagicWandsInfo(wand1);
+   strcpy(wand2.title, "Anguriel");
+   strcpy(wand2.author, "KapANA"); // ანგლახელის კოპი
+   strcpy(wand2.owner, "SHOSHIkapANA");
+   strcpy(wand2.soul, "Golden Dragon");
+   wand2.wand_id = 2;
 
+
+   /* print MagicWands info */
+  printMagicWandsInfo(wand1);
+  printMagicWandsInfo(wand2);
 
    return 0;
 }
