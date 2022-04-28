d92f152878d60b1c8f612b48fca306bdfc99e0f3
Meniskuri <giorgikapanadze9169@gmail.com>
2022-04-28 18:42:56 +0400

    sizmari qetize fulaze

--- me_first_/first_C_tutorial/Cstart/CsamiSaati_amocanebi/gagrdzeleba3.c
+++ me_first_/first_C_tutorial/Cstart/CsamiSaati_amocanebi/gagrdzeleba3.c
@@ -11,10 +11,6 @@
 #include <string.h>
 #include <errno.h>
 
-// გლობალური ცვლადები
-#define MAX_LENGTH 100  // სტრინგების მასივის შესაქმნელად სიგრძე
-#define NUM_STRINGS 100 // რამდენი სტრინგ ელემენტის შენახვა შეუძლია 2დ ერრეის
-
 int parametrebi_Brzanebis(char* brzaneba)
 {
   /* ფუნქციის აღწერა:
@@ -62,145 +58,34 @@ int parametrebi_Brzanebis(char* brzaneba)
   return cifria;
 }
 
-void civa_cxela_funqcia(double sacavi_statistikis[], int mtvleli, char sacavi_civa_cxela[][MAX_LENGTH], double secret)
-{
-  /*
-  ფუნქცია. რომელსაც მივაწვდი სტატისტიკის მასივს
-  გამოითვლის ჰოთ მინს და ქოლდ მაქს და დამიბეჭდვას
-  მგონი არ იქნება რთული. მთავარია თქვა რა გინდა.
-  ასევე ვაწვდი ცივა ცხელას მასივს საბეჭდად.
-  */
-  double coldMAX, hotMIN;
-  double chemMAX, chemMIN;
-
-  double sacavi_shida_stat_qveda[1000]; // საცავი ქვედა არეალში მოხვედრილი რიცხვებისთვის
-  int k = 0; // ინდექსების მთვლელი ქვედა არეალისთვის
-  double sacavi_shida_stat_zeda[1000]; // საცავი ზედა არეალში მოხვედრილი რიცხვებისთვის
-  int p = 0; // ინდექსების მთვლელი ზედა არეალისთვის
-
-  printf("საცნობი: %f - %s \n", sacavi_statistikis[mtvleli-1], sacavi_civa_cxela[mtvleli-1]);
-
-  chemMAX = chemMIN = sacavi_statistikis[0];
-
-  for (int i = 0; i < mtvleli; i++)
-  {
-    // მინ მაქს ის გამოთვლა მასივში
-    if (chemMIN > sacavi_statistikis[i])
-    chemMIN = sacavi_statistikis[i];
-    if (chemMAX < sacavi_statistikis[i])
-    chemMAX = sacavi_statistikis[i];
-
-    // ქვედა მასივის შექმნა რომელსიც უნდა გამოვითვალო მინ მაქსი ასევე
-    if (sacavi_statistikis[i] < secret)
-    {
-      sacavi_shida_stat_qveda[k] = sacavi_statistikis[i];
-      // printf("sacavi_shida_stat_qveda[%d] = %f \n", k, sacavi_shida_stat_qveda[k]);
-      k++;
-    }
-    // ქვედა მასივის შექმნა რომელსიც უნდა გამოვითვალო მინ მაქსი ასევე
-    if (sacavi_statistikis[i] > secret)
-    {
-      sacavi_shida_stat_zeda[p] = sacavi_statistikis[i];
-      // printf("sacavi_shida_stat_zeda[%d] = %f \n", p, sacavi_shida_stat_zeda[p]);
-      p++;
-    }
-
-    // მაქს ქვედა არეალისთვის
-    coldMAX = sacavi_shida_stat_qveda[0];
-    for (int i = 0; i < k; i++)
-    {
-      if (coldMAX < sacavi_shida_stat_qveda[i])
-      coldMAX = sacavi_shida_stat_qveda[i];
-    }
-    // მინ ზედა არეალისთვის
-    hotMIN = sacavi_shida_stat_zeda[0];
-    for (int i = 0; i < p; i++)
-    {
-      if (hotMIN > sacavi_shida_stat_zeda[i])
-      hotMIN = sacavi_shida_stat_zeda[i];
-    }
-  }
-
-  printf("(Main array) Minimum of array is : %f \n",chemMIN);
-  printf("(Main array) Maximum of array is : %f \n",chemMAX);
-
-  printf("(coldMAX) Maximum low diapason of array is : %f \n",coldMAX);
-  printf("(hotMIN) Minumum hight diapason of array is : %f \n",hotMIN);
-}
-
-/*
-- ინდექსების მოძებნა
-- ვაილ ლუპიდან გამოსვლა კითხვით
-- რენდომ სექრეტ რიცხვის დაგენერირება როცა ეგზე ფაილს გაუშვებ???
-*/
 
 int main()
 {
-  double secret = 1783;  // გამოსაცნობი რიცხვი
-  double mixvedra;       // კლავიატურიდან შეყვანილი რიცხვს სადაც ჩავწე და შევადარებ secret-ს
 
-  char brzaneba[51];     // სტრინგი რომელიც შემყავს კლავიატურიდან ვამოწმებ რიცხვია თუა არა და ვანიჭებ mixvedra-ს
-  bool brzanebaChek;     //  თუ ფუნქციამ დააბრუნა 1 ანუ რიცხვი შევიყვანეთ თუარადა დააბრუნებს 0 ს
-  char *eptr;            // ??? string pointer რაც გადაყავს დაბლში (მგონი)
-  int counter_while = 0; // ვაილის მთვლელი. როცა შეგვყავს რიცხვი. სხვა შემთხვევებს არ ავღრიცხავ
-  int counter_main  = 0;
-
-  double sacavi_statistikis[1000];                        // ვინახავ შეყვანილ რიცხვებს
-  char sacavi_civa_cxela[NUM_STRINGS][MAX_LENGTH] = {""}; // ყოველ შეყვანილ რიცხვზე ვიწერ ცივია თუ ცხელია
-
-  printf("================================== \n");
-  printf("ვთამაშობთ გამოცნობანას - წინასწარმეტყველების თამაში \n");
-  printf("შეყვანილია საიდუმლო secret ცვლადი, რომელიც უნდა გამოვიცნოთ \n");
-
-  while (true)
-  {
-    printf("================================== \n");
-    printf("შეიყვანეთ ნებისმიერი რიცხვი \n");
-    gets(brzaneba);
-    brzanebaChek = parametrebi_Brzanebis(brzaneba);
-
-    if (brzanebaChek == 1)
-    {
-      mixvedra = strtod(brzaneba, &eptr);
-      // printf("mixvedra = %f \n",mixvedra);
-
-      if (mixvedra == secret)
-      {
-        printf("თამაში დამთვრებულია \n");
-        break;
-      } else if (mixvedra < secret) {
-        strcpy(sacavi_civa_cxela[counter_while ],"cold");
-      } else {
-        strcpy(sacavi_civa_cxela[counter_while],"hot");
-      }
-      sacavi_statistikis[counter_while] = mixvedra;
-      counter_while++;
-    } else
-    {
-      printf("================================== \n");
-      printf("ERROR - შეიყვანეთ რიცხვი \n");
-    }
-    counter_main++;
-
-    printf("counter_main %d \n", counter_main);
-    printf("counter_while = %d \n", counter_while);
-
-    printf("========== \n");
-    civa_cxela_funqcia(sacavi_statistikis, counter_while, sacavi_civa_cxela, secret);
-  }
   return 0;
 }
 
 /*
-არჩილმა დამირეკა და მითხრა რომ ნუ აძინებ ტვინსო
-როცა ტვინი არ გყავს დაძინებული მაშინ გამოგივა ყველაფერიო
-ნუ გუდავ ტვინსო. ისე როცა არ გყავს გაგაუდული ტვინი.. ტვინი თვითონ ვერ გაჩერდებაო
-
-ვბრაზობ რომ ნელა მივდივარ წინ.
-გაიხსენე ლაი. ნელა სიარულით შორს წავა.
-ასე რომ იარე თუნდაც ნელა. მაგრამ იარე.
-ბოლოს ფოფხვაც რომ მოგწიოს გაიხსენე სიზმარი..
-რომ უნდა იარო..
-
-წინ დებით სიარული )))
+ლუკამ დამირეკა და შეიძლება ხვალ აბასთუმანში წავიდე
+დღეს დამესიზმრა რომ ვიყავი აბასთუმანში. ყველა იყო იკა ანი ბავშვები
+თორნე ვერა ბავშვები
+თიკუკა ნიკუშა მაშო ელენე
+ყველას ვეხუტებოდი.
+მერე დამესიზმრა ქეთი. მართვის მოწმობას იღებდა. დამესიზმრა რომ ჩუმად შევხდით
+ჩვენს ქუჩაზე. სადღაც სხვანაირი საბურთალო იყო და სხვანაირი პეკინი და სადღაც მანდ.
+ხილბოსტნის კაცი იყო და ყიდიდა რაღაცეებს. ქეთი ხატავდა და ისეთი რაღაც უნდა დაეხატა
+რაც მე გამახარებდა. მე კიდევ უნდა მემუშავა რომ ქეთის ხატვის ძალა ქონოდა.
+არადა ვიცი რომ ხატვა მაგრად ჭირდება. ნუ რათქმაუნდა ვერ მოვითმინე და ყურში ჩავჩურჩულე
+ქეთ მართლა აღარ გიყვარვარ? და სანამ რამეს მეტყოდა პასუხის შემეშინდა და გადავაკეთე კითხვა
+და არაა არაა მეგობრულ სიყვარულზე გეუბნებოდი :))) ჰოდა შემომხედა და მითხრა დებილი ხარო
+მოკლედ ქეთის დაურეკეს მერე იასნია და წასასვლელი იყო. წავიდა. ჰოდა მეც წავედი.
+მერე მე კომპიუტერთან ვიჯექი და თითქოს გვერძე მეჯდა მაგრამ ანუ არ იყო გვერძე მაგასთან სხვა სიტოაციები
+იჩითებოდა ჩემთან სხვა. ორივე ვჩალიჩობდით რაღაცას კომპიუტერით და ალბათ ვწერდით და მაგიტომ ჩანდა ისე
+თითქოს გვერდიგვერდ ვიჯექით.
+სამუშაო დღის ბოლოს დამირეკა fula - მ. (დღეს ვფიქრობდი რომ თუ ისევ ნახულობ დემონურ სიზმრებს
+შავ გეტოსთან ახლოს თეთრმა ანგელოზებმა მითხრეს და მივხდი რომ მართლა ეგრე მოხდა და ანუ ნუ კი პიზდეცია
+მაგრამ fula სთან სზიმრებში რომ კონტაქტი მაქვს ძაან ასწორებს - ფრაქტალური ქრისტე). მოკლედ დღესაც ვნახე რა
+სფიქსიც ვნახე. და მერე დიდი კამანდა. და წავედით ერთად ვინტის (მეტამფეტამინი) საჩალიჩოდ. და ვეუბნებოდი ყველაზე
+მაგარი გოგო მყავსთქო. ნუ თან მანდ მახსოვდა რომ ჩემი გოგო არ იყავი მაგრამ მაგ სიზმრაში... მოკლედ ნუ რა აზრი აქვს
+შენზე ვუყვებოდი ყველაზე მაგარი გოგოათქო. 
 */
