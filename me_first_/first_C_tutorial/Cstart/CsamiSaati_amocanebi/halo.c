0f10a6d49bcce812ac10f3db079fe2c0e43351e1
Meniskuri <giorgikapanadze9169@gmail.com>
2023-08-02 18:03:09 +0400

    pirveli programa isev

---
+++ me_first_/first_C_tutorial/Cstart/CsamiSaati_amocanebi/halo.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+
+
+int main()
+{
+  printf("gamarjoba samyaro");
+
+  return 0;
+}
