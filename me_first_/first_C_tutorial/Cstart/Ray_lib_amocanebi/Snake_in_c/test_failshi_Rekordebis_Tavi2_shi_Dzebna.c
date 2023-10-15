5b097efef93857432a6b00e7d46759c29be6f3b2
Meniskuri <giorgikapanadze9169@gmail.com>
2023-10-15 17:43:37 +0400

    test_failshi_rekordebis_tavi2_shi_dzebna shi failebze mushaobas vswavlob amovikitxe, exla gachedili var chaweraze sachiro adgilas

--- me_first_/first_C_tutorial/Cstart/Ray_lib_amocanebi/Snake_in_c/test_failshi_Rekordebis_Tavi2_shi_Dzebna.c
+++ me_first_/first_C_tutorial/Cstart/Ray_lib_amocanebi/Snake_in_c/test_failshi_Rekordebis_Tavi2_shi_Dzebna.c
@@ -34,12 +34,12 @@ int findLineNumber(char file_name[])
 void main()
 {
     char file_name[]  = {"test.txt"};
-    double Data[3];    // I'm interested in this information
-    char junk1, junk2; // junk variables to avoid first two characters
+    double Data[1000];    // I'm interested in this information
+    char junk1, junk2, junk3; // junk variables to avoid first two characters
     int lineNUMBER;
     int jami;
 
-    FILE * file = fopen("test.txt", "r"); // open file
+    FILE * file = fopen(file_name, "r"); // open file
 
     lineNUMBER = findLineNumber(file_name); /* ლაინების რაოდენობა რომ ვიცოდე */
 
@@ -50,8 +50,8 @@ void main()
         fscanf(file, "%s %s %lf\n", &junk1, &junk2, &Data[i]); //store info in Data array
         printf("%f\n", Data[i]); // print Data, just to check
       }*/
-      fscanf(file, "%s %s %lf\n", &junk1, &junk2, &Data[i]); //store info in Data array
-      printf("data [i] = %f; i = %d;i % 2 = %d;\n", Data[i],i,i % 2); // print Data, just to check
+      fscanf(file, "%s %s %s %lf\n", &junk1, &junk2,&junk3, &Data[i]); //store info in Data array
+      printf("data [i] = %f; i = %d;i procent gayofa 5 ze = %d;\n", Data[i],i,i % 5); // print Data, just to check
     }
     fclose(file);
 
@@ -59,17 +59,43 @@ void main()
     int VASO;
     double TATA;
     double KETO;
+    double DIANA;
+    double ETUNA;
+    int P = 3.14;
 
-    GIO = Data[0];
-    VASO = Data[1];
-    TATA = Data[2];
-    KETO = Data[3];
+    GIO   = Data[0];
+    VASO  = Data[1];
+    TATA  = Data[2];
+    KETO  = Data[3];
+    DIANA = Data[4];
+    ETUNA = Data[5];
 
     printf("Value of GIO is %d\n", GIO); // Print values to check
     printf("Value of VASO is %d\n", VASO);
     printf("Value of TATA is %f\n", TATA);
     printf("Value of KETO is %f\n", KETO);
+    printf("Value of DIANA is %f\n", DIANA);
+    printf("Value of ETUNA is %f\n", ETUNA);
     fclose(file);
 
     // თავიდან გავხსნა ფაილი და დავამატო T + rame (lineNUMBER)
+    file = fopen("test.txt", "a"); // open file ჩაწერისთვის (პის ვუმატებ ყოველ მეორეს)
+    for(int i = 0; i <= lineNUMBER; i++)
+    {
+      //
+      if (i % 2 == 0){
+        //
+        printf("dawere yoveli meore %f\n",Data[i]);
+        fprintf(file,"%d",Data[i]+3);
+      }
+
+    }
+    fclose(file);
+
 }
+
+/*
+exla gavxsna chemi faili da amovikitxo rekordebi sanam ar damtavrdeba failis gaxsna nu sxva testi davwero?
+Tu aq? es kai testia meroe testi xom ar shevqmna? sheidzleba. chai dalie moifiqre
+
+*/
