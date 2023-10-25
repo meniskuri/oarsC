4b333e037b675e7741355676eb5a089b0a3c4210
Meniskuri <giorgikapanadze9169@gmail.com>
2023-10-25 17:42:24 +0400

    test_for files gavakete snake_2 istvis fails vkitxulob da rekordebis modzebnaze var gadasuli da max ricxvis povnaze

--- me_first_/first_C_tutorial/Cstart/Ray_lib_amocanebi/Snake_in_c/TESTS_for_Files/test_snake.c
+++ me_first_/first_C_tutorial/Cstart/Ray_lib_amocanebi/Snake_in_c/TESTS_for_Files/test_snake.c
@@ -37,8 +37,8 @@ int main()
 {
     int kapi = 0;
     char file_name[]  = {"tavi2.txt"};
-    double Data[1000];    // I'm interested in this information
-    char junk1[80], junk2[80], junk3[80]; // junk variables to avoid first two characters
+    double Data[1000],Data2[1000];    // I'm interested in this information
+    char junk1[80], junk2[80], junk3[80], junk4[80]; // junk variables to avoid first two characters
     int lineNUMBER;
     int jami;
 
@@ -46,18 +46,30 @@ int main()
 
     lineNUMBER = findLineNumber(file_name); /* ლაინების რაოდენობა რომ ვიცოდე */
 
-    for(int i = 0; i <= lineNUMBER+6; i++) // each loop will read new line of file; i<3 for 3 lines in file
+    for(int i = 0; i <= lineNUMBER; i++) // each loop will read new line of file; i<3 for 3 lines in file
     {
-      fscanf(file, "%s %s %lf\n", &junk1, &junk2, &Data[i+1]); //store info in Data array
-      //printf("data [i+1] = %f; i = %d;i procent gayofa 4 ze = %d;\n", Data[i+1],i,i % 4); // print Data, just to check
+      if (i == 0 || i % 6 == 0)
+      {
+        // sataurs sxva wesit viwer failshi
+        fscanf(file, "%s %s %s %s %lf\n", &junk1, &junk2, &junk3,&junk4, &Data[i+1]);
+      } else {
+        // masivis shevseba sachiro infoti
+        fscanf(file, "%s %s %lf\n", &junk1, &junk2, &Data[i+1]);
+      }
+
+      // fscanf(file, "%s %s %lf\n", &junk1, &junk2, &Data[i+1]); //store info in Data array
+      // printf("data [i+1] = %f; i = %d;i procent gayofa 4 ze = %d;\n", Data[i+1],i,i % 4); // print Data, just to check
     }
     fclose(file);
+
+    // data2 masivis shevseba rekordebit da maximumis modzebna 
+
 
     printf("//////////////////// \n");
 
     // თავიდან გავხსნა ფაილი და დავამატო T + rame (lineNUMBER)
     file = fopen("test.txt", "r"); // open file ჩაწერისთვის (პის ვუმატებ ყოველ მეორეს)
-    for(int i = 0; i <= lineNUMBER + 6; i++)
+    for(int i = 0; i <= lineNUMBER; i++)
     {
       if (((i % (5 + kapi)) == 0) && i != 0)
       {
@@ -67,7 +79,13 @@ int main()
         // kapi = kapi + 7;
         //printf("(kapi)= %d\n",kapi);
       }
-      printf("data [i+1] = %f;\n", Data[i+1]);
+
+      if (i == 0 || i % 6 == 0)
+      {
+        //
+        printf("aq var\n");
+      }
+      printf("data [i+1] = %f; i = %d\n", Data[i+1],i);
 
     }
 
